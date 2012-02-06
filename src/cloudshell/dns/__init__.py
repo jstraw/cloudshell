a8f39710f9c7813462b529ff1c41105e31d97b70
Jason Straw <jason.straw@rackspace.com>
2012-02-06 11:43:09 -0600

    Created Domain level shell, finished dns shell (beta).

--- src/cloudshell/dns/__init__.py
+++ src/cloudshell/dns/__init__.py
@@ -1,10 +1,12 @@
 import cmd
+import shlex
 
 import clouddns.connection
 from clouddns.consts import uk_authurl
 
 from cloudshell.utils import color
 from cloudshell.base import base_shell
+from cloudshell.dns.domain import domain_shell
 
 
 class dns_shell(base_shell):
@@ -20,21 +22,69 @@ class dns_shell(base_shell):
             self.api = clouddns.connection.Connection(username=main_shell.username, 
                                                       api_key=main_shell.apikey)
         self.set_prompt(main_shell.username, ['DNS'])
+        self.domains = None
 #        self.prompt = color.set('blue') + 'CloudShell ' + \
 #                      color.set('cyan') + main_shell.username + \
 #                      color.set('blue') + ' DNS > ' + color.clear()
 
     def do_list(self, s):
         # Right now, the clouddns module doesn't do filtered search, get all of them
-        domains = self.api.get_domains()
-        if s != '' and s in domains:
+        if self.domains == None or 'refresh' in s:
+            self.domains = self.api.get_domains()
+        if s != '' and s in self.domains:
             print s, "is in the domain list"
         else:
-            for x in domains:
-                print x
+            for x in self.domains:
+                print x.id, ':', x.name
 
-    def do_exit(self, s):
-        return True
-    def do_EOF(self, s):
-        return True
+    def do_domain(self, s):
+        """Load the Domain Shell for listed domain"""
+        if self.domains == None:
+            self.domains = self.api.get_domains()
+        for x in self.domains:
+            if s in x.name:
+                d = self.api.get_domain(x.id)
+                self.domain = domain_shell(self, d)
+                self.domain.cmdloop()
+                break
+
+    def do_add(self, s):
+        """Create a Domain on your account:
+
+    Usage:
+        add <name> <ttl> <email Address>
+    """
+        args = shlex.split(s)
+        try:
+            self.api.create_domain(args[0], args[1], args[2])
+        except:
+            self.error("Failed to Create Domain")
+            raise
+
+    def do_delete(self, s):
+        """ Delete a domain from your account:
+    **** This will NOT ask you to confirm ****
+
+    Usage:
+        delete <domain id>
+    """
+        if self.domains == None or 'refresh' in s:
+            self.domains = self.api.get_domains()
+        if s in self.domains:
+            for x in self.domains:
+                if s in x.name:
+                    d = x.id
+                    break
+        else:
+            d = s
+
+        try:
+            self.api.delete_domain(d)
+        except:
+            self.error("Failed to Delete Domain")
+            raise
+
+
+    def do_python(self, s):
+        import pdb; pdb.set_trace()
 
---
+++ src/cloudshell/dns/domain.py
@@ -0,0 +1,87 @@
+import cmd
+import datetime
+import shlex
+
+import prettytable
+import clouddns.connection
+from clouddns.consts import uk_authurl
+
+from cloudshell.utils import color
+from cloudshell.base import base_shell
+
+
+class domain_shell(base_shell):
+    def __init__(self, dns_shell, domain):
+        base_shell.__init__(self)
+        self.dns_shell = dns_shell
+        self.domain = domain
+        self.set_prompt(self.dns_shell.main_shell.username, ['DNS', domain.name])
+        self.records = None
+
+    def do_list(self, s):
+        "List the records on this domain."
+        # Right now, the clouddns module doesn't do filtered search, get all of them
+        if self.records == None or 'refresh' in s:
+            self.records = self.domain.get_records()
+        rlist = prettytable.PrettyTable(['ID', 'FQDN', 'Type', 'IP/Alias',
+                                         'TTL', 'Created', 'Updated'])
+        rlist.set_field_align('ID', 'l')
+        rlist.set_field_align('FQDN', 'l')
+        rlist.set_field_align('Type', 'c')
+        rlist.set_field_align('IP/Alias', 'l')
+        rlist.set_field_align('TTL', 'l')
+        rlist.set_field_align('Created', 'l')
+        rlist.set_field_align('Updated', 'l')
+        for x in self.records:
+            rlist.add_row([x.id, x.name, x.type, x.data, x.ttl, 
+                           x.created.strftime('%c'), 
+                           x.updated.strftime('%c')])
+        rlist.printt()
+
+    def do_add(self, s):
+        """Create a new record:
+
+    Usage:
+        add <type> <fqdn> <IP/Alias> <TTL> [MX Priority]
+    """
+        args = shlex.split(s)
+        if args[0].upper() == 'MX' and not len(args) == 5:
+            self.error("You need an MX Priority when you make a MX record")
+        if len(args) < 5:
+            args.append(None)
+        try:
+            self.domain.create_record(args[1], args[2], args[0].upper(), 
+                                      args[3], args[4])
+        except:
+            pass
+    do_create = do_add
+
+    def do_delete(self, s):
+        """Delete a record:
+
+    Usage:
+        delete <Record ID>
+    """
+        try:
+            self.domain.delete_record(s)
+        except:
+            pass
+    do_del = do_delete
+
+    def do_ttl(self, s):
+        """Configure Domain-wide TTL"""
+        try:
+            self.domain.update(ttl=s)
+        except:
+            pass
+
+    def do_email(self, s):
+        """Configure Email Address contact for domain"""
+        try:
+            self.domain.update(emailAddress=s)
+        except:
+            pass
+
+    def do_python(self, s):
+        import pdb; pdb.set_trace()
+
